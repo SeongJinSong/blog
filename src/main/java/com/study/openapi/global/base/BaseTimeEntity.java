e26e0db8df07e233ce086cbcec128a980e565390
seongjin_song <seongjin_song@tmax.co.kr>
2022-09-17 19:42:05 +0900

    add : BaseTimeEntity, ResponseWrapper 추가

---
+++ src/main/java/com/study/openapi/global/base/BaseTimeEntity.java
@@ -0,0 +1,23 @@
+package com.study.openapi.global.base;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public class BaseTimeEntity {
+    @Column(updatable = false)
+    @CreatedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime modifiedAt;
+}
