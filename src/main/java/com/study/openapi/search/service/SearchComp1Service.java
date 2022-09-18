589cf32b148f2c018a361d9d96cbb3e88bcaf170
seongjin_song <seongjin_song@tmax.co.kr>
2022-09-18 23:07:43 +0900

    chore : 코드 점검

--- src/main/java/com/study/openapi/search/service/SearchComp1Service.java
+++ src/main/java/com/study/openapi/search/service/SearchComp1Service.java
@@ -26,7 +26,12 @@ public class SearchComp1Service implements SearchService{
         searchHistoryService.saveRequest(request);
 
         String key = getComp1ApiRedisKey(httpservletRequest.getRequestURI(), httpservletRequest.getQueryString());
-        //TODO @Cacheable을 적용하여 분기 삭제
+        /*
+            TODO
+             1. @Cacheable을 적용하여 분기 삭제
+             2. API자체를 캐싱하는건 Client단에서 어울릴만한 일이다. reactQuery가 그런비슷한 역할을 하기도한다.
+             서버단에서는 뭔가 서버단에서 어울릴만한 캐싱 알고리즘을 찾아보아야 할 것 같다.
+         */
         SearchResponse<Blog> response = redisService.getApiResultCache(key);
         if(response==null){
             //api 호출
