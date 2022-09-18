040ed92570be7444d2fcf97da61e3b17d250b0d6
seongjin_song <seongjin_song@tmax.co.kr>
2022-09-18 22:26:24 +0900

    feat : developer request 제약조건대로 설정

--- src/main/java/com/study/openapi/global/common/SearchRequest.java
+++ src/main/java/com/study/openapi/global/common/SearchRequest.java
@@ -1,9 +1,11 @@
 package com.study.openapi.global.common;
 
+import com.sun.istack.Nullable;
 import lombok.*;
 
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter //setter가 있어야 argumentResolver에서 객체로 매핑 가능한거 같다.
@@ -13,8 +15,12 @@ import javax.validation.constraints.NotBlank;
 @ToString
 public class SearchRequest {
     @NotBlank
+    @NotNull
     private String query;
+    @Nullable
     private String sort;
+    @Nullable
     private Integer page;
+    @Nullable
     private Integer size;
 }
